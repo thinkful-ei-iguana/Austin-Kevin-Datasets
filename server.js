86d8223497224a8d09f78f85ae2be8b6dc365565
Kevin Moore <moore.kevin.p@gmail.com>
2019-12-03 15:48:19 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const morgan = require('morgan')
+
+const app = express()
+
+app.use(morgan('dev'))
+
+const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
+
+function handleGetTypes(req, res) {
+
+}
+
+app.get('/types', handleGetTypes)
+
+const PORT = 8000
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`)
+})
